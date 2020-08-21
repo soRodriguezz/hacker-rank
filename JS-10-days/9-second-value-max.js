8b42b93e2d6ff8e941098f481f08f5b91e59adbb
sebastian <s.rodriguezzapata01@gmail.com>
2020-08-21 18:44:47 -0400

    second-max

--- JS-10-days/9-second-value-max.js
+++ JS-10-days/9-second-value-max.js
@@ -1,17 +1,10 @@
 /**
  * * Encontrar el segundo valor mas alto del arreglo
  * * el arreglo trae '2,3,6,6,5'
- */
-
+*/
 function getSecondLargest(nums) {
-    // eliminar los valores repetidor del arreglo
-    function onlyUnique(value, index, self) {
-        return self.indexOf(value) === index;
-    }
-
-    // Pasar los valores unicos y ordenarlos de menor a mayor
-    let numberMax = nums.filter(onlyUnique).sort();
-    // mostrar el penultimo resultado
-    return numberMax[numberMax.length - 2]
+        // Complete the function
+        let results = nums.sort((a, b) => a - b)
+            .filter((el, index, array) => index === array.indexOf(el));
+        return results[results.length - 2];
 }
-
